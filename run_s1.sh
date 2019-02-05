51477ee6e24d268a0b9780dbff523db7697728eb
xyn11 <43618917+xyn11@users.noreply.github.com>
2019-02-05 11:00:16 -0800

    Delete run_s1.sh

--- run_s1.sh
+++
@@ -1,9 +0,0 @@
-
-/usr/local/spark/bin/spark-submit --master spark://ec2-35-155-195-242.us-west-2.compute.amazonaws.com:7077 \
---packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.2 \
---num-executors 4 \
---executor-cores 3 \
---executor-memory 2G \
---conf spark.scheduler.mode=FAIR \
---conf spark.default.parallelism=2 \
-~/Insight-DE19A/s1yelp.py
